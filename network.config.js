256fee121778537cb636b02bac845529417d1754
Sujon <160887695+sujon12210@users.noreply.github.com>
2026-01-18 14:20:34 +0600

    Create network.config.js

---
+++ network.config.js
@@ -0,0 +1,8 @@
+export const NETWORKS = {
+  ethereum: "https://rpc.ankr.com/eth",
+  base: "https://rpc.ankr.com/base",
+  polygon: "https://rpc.ankr.com/polygon",
+  arbitrum: "https://rpc.ankr.com/arbitrum"
+};
+
+export const NETWORK = "base";
